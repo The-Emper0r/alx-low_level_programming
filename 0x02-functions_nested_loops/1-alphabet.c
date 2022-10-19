4f137d5b43bb254a9cea73b656eedd599201cedb
The <ojinkeyac@gmail.com>
2022-10-18 19:30:21 -0700

    print alphabets

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,10 +2,13 @@
 /**
 * print_alphabet - prints the alphabet in lowercase, followed by a new line
 */
+
 void print_alphabet(void);
 {
 	char alpha;
+
 	alpha = 'a';
+
 	while (alpha <= 'z')
 	{
 		_putchar(alpha);
